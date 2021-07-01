19a485323c14e434331cf5527849f7160aaf4dd8
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-07-01 19:46:49 +0530

    Express and basic product route setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("API running")
+})
+
+app.get("/api/product", (req, res) => {
+    res.json(products)
+})
+
+app.get("/api/product/:id", (req, res) => {
+    var product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(5000, () => {
+    console.log('Server running on port 5000');
+})
