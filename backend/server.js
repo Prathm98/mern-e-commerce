0d5b0e88adcdcfec710acc03dac79794191c3c09
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-07-01 20:45:31 +0530

    Environment Variable & ES for backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import products from './data/products.js'
+import dotenv from 'dotenv'
 
 const app = express()
+
+dotenv.config()
 
 app.get("/", (req, res) => {
     res.send("API running")
@@ -16,6 +19,9 @@ app.get("/api/products/:id", (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
 })
