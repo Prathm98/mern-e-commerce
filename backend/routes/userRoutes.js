cbfe515d3be08a4d254ac49dcd9ea42745d960a0
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-07-07 15:55:26 +0530

    User Registration route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 import mongoose from "mongoose"
-import { authUser, getUserProfile } from '../controller/usersController.js'
+import { authUser, getUserProfile, registerUser } from '../controller/usersController.js'
 import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.post('/', registerUser)
 router.route('/profile').get(protect, getUserProfile)
 
 export default router
