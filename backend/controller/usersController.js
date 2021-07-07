2a4d5c3a4e8d0885eda71ad4480345ed66a89a80
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-07-07 14:45:55 +0530

    Generate JWT token

--- backend/controller/usersController.js
+++ backend/controller/usersController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Authetication of user and token issue
 // @route   GET /api/users/login
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }else{
         res.status(401)
