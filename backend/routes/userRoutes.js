9573ecce94bbb6fbff8dd5b22abdf959f417011e
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-07-07 15:22:02 +0530

    Custom Authorization middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
 import mongoose from "mongoose"
-import { authUser } from '../controller/usersController.js'
+import { authUser, getUserProfile } from '../controller/usersController.js'
+import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
